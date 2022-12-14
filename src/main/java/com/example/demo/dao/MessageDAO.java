e9b366b4a3b870d0681ef368efd301dec7fa56b6
박세진 <crazywarden@naver.com>
2022-12-14 15:31:39 +0900

    4th : 변수명 통일 (파일추가)

---
+++ src/main/java/com/example/demo/dao/MessageDAO.java
@@ -0,0 +1,18 @@
+package com.example.demo.dao;
+
+import com.example.demo.mapper.MessageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MessageDAO {
+
+    @Autowired
+    MessageMapper messageMapper;
+
+
+    public int insert(String msg) {
+        System.out.println("dao 도착");
+        return messageMapper.insert(msg);
+    }
+}
